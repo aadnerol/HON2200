8014f339ca226e732df92b497b6f3bf5844a3bc4
aadnero <aadnero@ulrik.uio.no>
2026-03-12 10:52:05 +0100

    Implementert og testet vekter og biaser

--- oblig/code/methods.py
+++ oblig/code/methods.py
@@ -10,3 +10,23 @@ def sigmoid(z: float) -> float:
         float: Funksjonsverdi
     """
     return np.exp(z) / (1 + np.exp(z))
+
+def vekter_bias(rad: int, kol: int, seed: int = None):
+    """Lager vekter og biaser.
+
+    Args:
+        rad (int): Antall rader i designmatrisen
+        kol (int): Antall kolonner i designmatrisen
+        seed (int): Seed for reproduserbarhet. Optional
+
+    Returns:
+        Vekter W (matrise) og biaser b (vektor)
+    """
+    if seed: 
+        rng = np.random.default_rng(seed)
+        W = rng.random((kol, 1))
+        b = rng.random((rad))
+    else: 
+        W = np.random.randn(kol, 1)
+        b = np.random.randn(rad)
+    return W, b
